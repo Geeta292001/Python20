7bcc135ffe0647c2ac6cfd8756d179d28b710388
AmulyaG27 <amulyag27@gmail.com>
2025-01-02 13:20:37 +0530

    regarding list operations

---
+++ list.py
@@ -0,0 +1,46 @@
+# LIST :
+#        A list is a mutable, and allow dupicate elements.
+#        It is a ordered collection of elements that can store items of different data types.
+#        It is defined by square
+#        EXAMPLE:
+#               [1,2,20.12,"Apple",66.44]
+#
+#
+# ADVANTAGES OF LIST:
+#
+#      Versatility
+#               They can store heterogeneous data types  (sring,interger,etc.)
+#      Dynamic Size
+#               List can grow shrink dynamically without requring a fixed size.
+#
+# DISADVANTAGES OF LIST:
+#
+#     Memory Overhead
+#               Lists consume more memory as they store additional metadata like type inforamtion and size.
+#     Slower for some operation
+#               Accessing and modifying elements can be slower compared to arrays in other languages due to dynamic typing.
+#
+#     EXAMPLES:
+
+# # Create a list of mixed data types
+#my_list = [10, "Python", 3.14, True]
+
+# # Access elements using indexing
+#print(my_list[0])  # Output: 10
+#print(my_list[-1]) # Output: True
+
+
+
+# # Create a list of numbers
+# numbers = [1, 2, 3, 4, 5]
+#
+# # Add an element to the list
+# numbers.append(6)  # List becomes [1, 2, 3, 4, 5, 6]
+#
+# # Remove an element
+# numbers.remove(3)  # List becomes [1, 2, 4, 5, 6]
+#
+# # Sort the list
+# numbers.sort()  # List becomes [1, 2, 4, 5, 6]
+#
+# print(numbers)
